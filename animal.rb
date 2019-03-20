a8969b23edff696c473894d49b02ea2b195b2ffb
shakil <shakilnsu2018@gmail.com>
2019-03-20 22:00:58 +0600

    Initial commit

---
+++ animal.rb
@@ -0,0 +1,11 @@
+class Animal
+  attr_accessor :name, :age, :trair
+end
+firstAnimal = Animal.new
+
+firstAnimal.name = "cow"
+firstAnimal.age = 36
+firstAnimal.trair = "anoying"
+
+puts firstAnimal.name
+puts firstAnimal.trair
